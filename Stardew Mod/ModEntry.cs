6522cd171235d0dea697fc182ec85937bd4cfae6
Chris Struck <chrisblammo123@gmail.com>
2021-07-05 00:25:45 -0500

    should be mostly functional, test sometime then start actual development

---
+++ Stardew Mod/ModEntry.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace StardewMod
+{
+	// The mod entry point.
+
+
+	/**
+	 * @class ModEntry
+	 * @description The mod entry point.
+	 */
+	public class ModEntry : Mod
+	{
+		// Public Methods
+
+
+
+		/**
+		 * @function Entry
+		 * @description Called when SMAPI loads the mod.
+		 * @param helper Gives access to SMAPI.
+		 */
+		public override void Entry(IModHelper helper)
+		{
+			// Creates a event handler for button presses by a player.
+			helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+		}
+
+
+		// Private Methods
+
+		/**
+		 * @function OnButtonPressed
+		 * @description Handles the event of a button being pressed by a player.
+		 * @param sender Player who triggered the event.
+		 * @param e The data from the event.
+		 */
+		private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+		{
+			// ignore if player hasn't loaded a save yet
+			if (!Context.IsWorldReady)
+				return;
+
+			// print button presses to the console window
+			this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+		}
+	}
+}
